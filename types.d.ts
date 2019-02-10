1af67a9f273988de01e1a69284e3a2fed825e8bc
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-02-10 21:48:47 +0100

    Use UnwrapSource type instead of its copy

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,5 @@
 import { Source } from 'callbag';
 
-type SourceType<T extends Source<any>> = T extends Source<infer O> ? O : never
-
 export default function merge<T extends Source<any>[]>(
   ...sources: T
-): Source<SourceType<T[number]>>;
+): Source<UnwrapSource<T[number]>>;
